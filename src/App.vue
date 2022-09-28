e973da1cf8f72b54882a47062c83483c55f61c18
Tempestfury11 <swiftyblue11@gmail.com>
2022-09-28 15:08:30 +0200

    health tracker

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <SideBar />
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import SideBar from './components/SideBar';
+
+export default {
+    name: "App",
+    components: { SideBar },
+};
+</script>
